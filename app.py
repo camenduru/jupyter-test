d680a4603983edba1380e8951690e5f070dbc11d
camenduru <54370274+camenduru@users.noreply.github.com>
2023-11-03 17:44:25 +0300

    test

--- app.py
+++ app.py
@@ -1,4 +1,2 @@
-from notebook.notebookapp import NotebookApp
-app = NotebookApp()
-app.initialize(["--port", str(7860), "--ip", "0.0.0.0", "--NotebookApp.token", "", "--no-browser", "--NotebookApp.tornado_settings", "{'headers': {'Content-Security-Policy': 'frame-ancestors *'}}"])
-app.start()
+from notebook.app import main
+main(["--port", str(7860), "--ip", "0.0.0.0", "--NotebookApp.token", "", "--no-browser", "--NotebookApp.tornado_settings", "{'headers': {'Content-Security-Policy': 'frame-ancestors *'}}"])
